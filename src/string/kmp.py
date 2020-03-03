a55dcbd54173bedd4b82225a0a948a05cf89b107
grayondream <grayondream@outlook.com>
2020-03-03 23:13:38 +0800

    kmp done

--- src/string/kmp.py
+++ src/string/kmp.py
@@ -1,35 +1,33 @@
-class dfa_kmp(object):
-    def __init__(self, pat, txt):
-        self.dfa = []
+
+def create_dfa(pat):
+    pat_len = len(pat)
+    r = 256
+    dfa = [[0 for i in range(pat_len)] for j in range(r)]
+    dfa[ord(pat[0])][0] = 1
+    i = 0
+    j = 1
+    while j < pat_len:
+        for c in range(r):
+            dfa[c][j] = dfa[c][i]
+            
+        dfa[ord(pat[j])][j] = j + 1
+        i = dfa[ord(pat[j])][i]
+        j += 1
+    return dfa
     
-    def create_dfa(self, pat):
-        pat_len = len(pat)
-        r = 256
-        self.dfa = [pat_len * [0]] * r
-        self.dfa[pat[0]][0] = 1
-        i = 0
-        j = 1
-        while j < pat_len:
-            for c in range(r):
-                self.dfa[c][j] = self.dfa[c][i]
-                
-            self.dfa[pat[j]][j] = j + 1
-            i = self.dfa[pat[j]][i]
-            j += 1
+def search(txt, pat, dfa):
+    i = 0
+    j = 0
+    n = len(txt)
+    m = len(pat)
+    while i < n and j < m:
+        j = dfa[ord(txt[i])][j]
+        i += 1
     
-    def search(self, txt, pat):
-        i = 0
-        j = 0
-        n = len(txt)
-        m = len(pat)
-        while i < n and j < m:
-            j = self.dfa[txt[i]][j]
-            i += 1
-        
-        if j == m:
-            return i - m
-        
-        return n
+    if j == m:
+        return i - m
+    
+    return n
         
     
 def get_next(pat):
@@ -72,5 +70,14 @@ def kmp_next_test():
     print(txt[ret:])
     
 
+def kmp_dfa_test():
+    txt = 'abababac'
+    pat = 'ababac'
+    dfa = create_dfa(pat)
+    ret = search(txt, pat, dfa)
+    print(ret)
+    print(txt[ret:])
+    
 def kmp_test():
-    kmp_next_test()
+    #kmp_next_test()
+    kmp_dfa_test()
