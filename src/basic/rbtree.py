e7089ec7cdea7f98532fac8acb3e10ac6a4762b6
grayondream <grayondream@outlook.com>
2020-03-18 12:29:09 +0800

    红黑树

--- src/basic/rbtree.py
+++ src/basic/rbtree.py
@@ -78,7 +78,7 @@ class rbtree(object):
             
         if rbtree.is_red(node.left) and rbtree.is_red(node.right):
             node = rbtree.flip_color(node)
-        elif rbtree.is_red(node.left) and rbtree.is_red(node.lef.left):
+        elif rbtree.is_red(node.left) and rbtree.is_red(node.left.left):
             node = rbtree.rotate_right(node)
         elif rbtree.is_red(node.right) and not rbtree.is_red(node.left):
             node = rbtree.rotate_left(node)
@@ -90,9 +90,144 @@ class rbtree(object):
         root = rbtree.put(root,key)
         root.color = black
         
+    @staticmethod
+    def move_red_left(node):
+        '''
+        @brief  处理2结点
+        '''
+        node = rbtree.flip_color(node)
+        if rbtree.is_red(node.right.left):  #当前结点并不是三个2结点，右结点是一个3结点
+            node.right = rbtree.rotate_right(node.right)
+            node = rbtree.rotate_left(node)
+            node = rbtree.flip_color(node)
+        
+        return node
+        
+    @staticmethod
+    def del_min(root):
+        '''
+        @brief  删除红黑树中的最小值
+        @param  root    红黑树的根结点
+        '''
+        if not rbtree.is_red(root.left) and not rbtree.is_red(root.right):
+            root.color = red;
+        
+        root = rbtree.del_min_node(root)
+        if root is not None:
+            root.color = black
+            
+    @staticmethod
+    def del_min_node(node):
+        '''
+        @brief 
+        '''
+        if root.left is None:
+            return None
+            
+        if not rbtree.is_red(node.left) and not rbtree.is_red(node.left.left):
+            #只会对2结点进行变换处理
+            node = rbtree.move_red_left(node)
+            
+        node.left = rbtree.del_min_node(node.left)
+        return rbtree.balance(node)
+        
+    @staticmethod
+    def balance(node):
+        '''
+        @brief 进行颜色平衡，将临时的4结点分解
+        @param  node    需要进行调整的结点过程基本和插入操作中的颜色转换完全相同
+        '''
+        if rbtree.is_red(node.right):
+            node = rbtree.rotate_left(node)
+        
+        if rbtree.is_red(node.right) and not rbtree.is_red(node.left):
+            node = rbtree.rotate_left(node)
+            
+        if rbtree.is_red(node.left) and rbtree.is_red(node.left.left):
+            node = rbtree.rotate_right(node)
+            
+        if rbtree.is_red(node.left) and rbtree.is_red(node.right):
+            rbtree.flip_color(node)
+            
+        return node
+        
+    @staticmethod
+    def move_red_right(node):
+        rbtree.flip_color(node)
+        if rbtree.is_red(node.left.left):
+            node = rbtree.rotate_right(node)
+            node = rbtree.flip_color(node)
+            
+        return node
+        
+    @staticmethod
+    def del_max_node(node):
+        if rbtree.is_red(node.left):
+            node = rbtree.rotate_right(node)
+        
+        if node.right is None:
+            return None
+            
+        if not rbtree.is_red(node.right) and not rbtree.is_red(node.right.left):
+            node = rbtree.move_red_right(node)
+        
+        node.right = rbtree.del_max_node(node.right)
+        return rbtree.balance(node)
+        
+    @staticmethod
+    def del_max(root):
+        if not rbtree.is_red(root.right) and not rbtree.is_red(root.left):
+            root.color = red
+            
+        root = rbtree.del_max_node(root)
+        if root is not None:
+            root.color = black
+    
+    @staticmethod
+    def min(node):
+        if node is None:
+            return None
+        while node.left is not None:
+            node = node.left
+        
+        return node
+        
+    @staticmethod
+    def delete_node(node, key):
+        if key < node.key:
+            if not rbtree.is_red(node.left) and not rbtree.is_red(node.left.left):
+                node = rbtree.move_red_left(node)
+            
+            node.left = rbtree.delete_node(node.left, key)
+        else:
+            if rbtree.is_red(node.left):
+                node = rbtree.rotate_right(node)
+            
+            #删除的结点是底部
+            if key == node.key and node.right is None:
+                return None
+            
+            if not rbtree.is_red(node.right) and not rbtree.is_red(node.right.left):
+                node = rbtree.move_red_right(node)
+            
+            #将当前树最小值和当前结点交换删除最小值即可
+            if key == node.key:
+                node.key = rbtree.min(node.right).key
+                node.right = rbtree.del_min(node.right)
+            else:
+                node.right = rbtree.delete_node(node.right, key)
+    
+        return rbtree.balance(node)
         
         
+    @staticmethod
+    def delete(root, key):
+        if not rbtree.is_red(root.right) and not rbtree.is_red(root.left):
+            root.color = red
         
+        root = rbtree.delete_node(root, key)
+        if root is not None:
+            root.color = black
         
         
         
